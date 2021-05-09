8f780123079b11d01751ffabdb444af36452f2a4
Josh Yi <jyi468@gmail.com>
2021-05-09 11:56:19 -0400

    Add nextJs router

--- pages/portfolio/[projectid].js
+++ pages/portfolio/[projectid].js
@@ -1,4 +1,13 @@
+import {useRouter} from 'next/router';  // use withRouter for class based
+
 function PortfolioProjectPage() {
+    const router = useRouter();
+
+    console.log(router.pathname);
+    console.log(router.query);  // gives us data from path (projectid)
+    // Can send request to backend server
+    // Fetch data with id of router.query.projectid
+
     return (
         <div>
             <h1>The Portfolio Project Page</h1>
