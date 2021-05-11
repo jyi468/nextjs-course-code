e662a46f27659a6edfbda0a775efe32d84f95538
Josh Yi <jyi468@gmail.com>
2021-05-11 18:05:08 -0400

    Create event list and event item components. Show events on home page using dummy data

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
+import {getFeaturedEvents} from "../dummy-data";
+import EventList from "../components/events/event-list";
+
 const HomePage = () => {
+    const featuredEvents = getFeaturedEvents();
     return (
         <div>
-            <h1>Featured Events</h1>
+            <EventList items={featuredEvents}/>
         </div>
     );
 };
