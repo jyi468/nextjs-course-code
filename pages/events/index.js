dd601e66d60728d3fd6d6173ef1ac21cb332b984
Josh Yi <jyi468@gmail.com>
2021-05-28 19:50:18 -0400

    Add filter form

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,9 +1,12 @@
 import {getAllEvents} from "../../dummy-data";
 import EventList from '../../components/events/event-list';
+import EventsSearch from "../../components/events/events-search";
+
 const AllEvents = () => {
     const events = getAllEvents();
     return (
         <div>
+            <EventsSearch/>
             <EventList items={events}/>
         </div>
     );
