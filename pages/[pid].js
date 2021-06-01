eba472d62a68d170a088ae4a690e1acd68256bb2
Josh Yi <jyi468@gmail.com>
2021-06-01 19:30:18 -0400

    Add link on products and pregenerate

---
+++ pages/[pid].js
@@ -0,0 +1,34 @@
+import {Fragment} from "react";
+import path from 'path';
+import fs from 'fs/promises';
+
+function ProductDetailPage(props) {
+    const {loadedProduct} = props;
+    // Pure react way is to use useEffect to make an API call but then page is not there when page initially rendered
+    return <Fragment>
+        <h1>{loadedProduct.title}</h1>
+        <p>{loadedProduct.description}</p>
+    </Fragment>
+}
+
+export async function getStaticProps(context) {
+    // params are keys for path
+    // Could be extracted through useRouter but then would only be in the component and making calls through browser
+    // For pre-rendering API calls, we need information here
+    const {params} = context;
+
+    const productId = params.pid;
+
+    const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json');
+    const jsonData = await fs.readFile(filePath);
+    const data = JSON.parse(jsonData);
+
+    const product = data.products.find(product => product.id === productId);
+    return {
+        props: {
+            loadedProduct: product
+        }
+    };
+}
+
+export default ProductDetailPage;
