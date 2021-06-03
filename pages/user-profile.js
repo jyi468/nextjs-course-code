21d50c591a1b40a0a2868174c1ca119e88682611
Josh Yi <jyi468@gmail.com>
2021-06-03 18:01:30 -0400

    Get req and res objects from context

--- pages/user-profile.js
+++ pages/user-profile.js
@@ -7,6 +7,8 @@ export default UserProfilePage;
 // Called for every incoming request.
 // Only executes after deployment, no pre-rendering. And also must be triggered by user
 export async function getServerSideProps(context) {
+    // req, res default objects from node for request and responses
+    const {params, req, res} = context;
     return {
         props: {
             username: 'Josh'
