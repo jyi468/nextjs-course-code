edeed8062e454a78b6131e25de964ce57cdbcd5b
jyi <joshua.yi@amadeus.com>
2021-06-04 13:26:26 -0400

    Use SWR to refactor fetch

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,35 +1,33 @@
 import {useEffect, useState} from 'react';
+import useSWR from 'swr';
 
 function LastSalesPage() {
     const [sales, setSales] = useState();
-    const [isLoading, setIsLoading] = useState(false);
+    // const [isLoading, setIsLoading] = useState(false);
+
+    const {data, error} = useSWR('https://nextjs-course-f4281-default-rtdb.firebaseio.com/sales.json');
 
     useEffect(() => {
-        setIsLoading(true);
-        // .json is required by firebase
-        fetch('https://nextjs-course-f4281-default-rtdb.firebaseio.com/sales.json'
-        ).then(response => response.json())
-            .then(data => {
-                const transformedSales = [];
-                for (const key in data) {
-                    transformedSales.push({
-                        id: key,
-                        username: data[key].username,
-                        volume: data[key].volume
-                    })
-                }
+        if (data) {
+            const transformedSales = [];
+            for (const key in data) {
+                transformedSales.push({
+                    id: key,
+                    username: data[key].username,
+                    volume: data[key].volume
+                })
+            }
 
-                setSales(transformedSales);
-                setIsLoading(false);
-            });
-    }, []);
+            setSales(transformedSales);
+        }
+    }, [data]);
 
-    if (isLoading) {
-        return <p>Loading...</p>;
+    if (error) {
+        return <p>Failed to load.</p>;
     }
 
-    if (!sales) {
-        return <p>No data yet</p>;
+    if (!data || !sales) {
+        return <p>Loading...</p>;
     }
 
     return (
