3e329069cfca3d4aa58708efe9db32c3298a9b36
Josh Yi <jyi468@gmail.com>
2021-06-05 11:28:55 -0400

    Combine pre-rendering and client side fetching

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,8 +1,8 @@
 import {useEffect, useState} from 'react';
 import useSWR from 'swr';
 
-function LastSalesPage() {
-    const [sales, setSales] = useState();
+function LastSalesPage(props) {
+    const [sales, setSales] = useState(props.sales); // Use pre-rendered sales from the server
     // const [isLoading, setIsLoading] = useState(false);
 
     const {data, error} = useSWR('https://nextjs-course-f4281-default-rtdb.firebaseio.com/sales.json');
@@ -26,7 +26,7 @@ function LastSalesPage() {
         return <p>Failed to load.</p>;
     }
 
-    if (!data || !sales) {
+    if (!data && !sales) {
         return <p>Loading...</p>;
     }
 
@@ -37,6 +37,22 @@ function LastSalesPage() {
             </li>)}
         </ul>
     );
+}
+
+export async function getStaticProps() {
+    // .json is required by firebase
+    const response = await fetch('https://nextjs-course-f4281-default-rtdb.firebaseio.com/sales.json');
+    const data = await response.json();
+    const transformedSales = [];
+    for (const key in data) {
+        transformedSales.push({
+            id: key,
+            username: data[key].username,
+            volume: data[key].volume
+        })
+    }
+
+    return {props: {sales: transformedSales}, revalidate: 10};
 }
 
 export default LastSalesPage;
