3bdbde221562e140bb4dda0b55ac01ab63e8350d
jyiwm <joshua.yi@warnermedia.com>
2021-07-16 12:31:00 -0400

    Update index.js to prepare for api route handling

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,30 @@
+import {useRef} from 'react';
+
+const emailInputRef = useRef();
+const feedbackInputRef = useRef();
+
+function submitFormHandler(event) {
+    event.preventDefault();
+
+    const enteredEmail = emailInputRef.current.value;
+    const enteredFeedback = feedbackInputRef.current.value;
+}
+
 function HomePage() {
   return (
     <div>
       <h1>The Home Page</h1>
+        <form onSubmit={submitFormHandler}>
+            <div>
+                <label htmlFor='email'>Your Email Address</label>
+                <input type='email' ref={emailInputRef}/>
+            </div>
+            <div>
+                <label htmlFor='feedback'>Your Feedback</label>
+                <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
+            </div>
+            <button>Send Feedback</button>
+        </form>
     </div>
   );
 }
