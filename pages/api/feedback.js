fd6196af35526bc5ea2a3f851b907524e0bb8561
jyiwm <joshua.yi@warnermedia.com>
2021-07-16 12:39:49 -0400

    Parse request and write to file code

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,28 @@
+import fs from 'fs';
+import path from 'path';
+
 function handler(req, res) {
-    res.status(200).json({message: 'This works!'});
+    if (req.method === 'POST') {
+        // req.body is already parsed by nextjs for us
+        const email = req.body.email;
+        const feedbackText = req.body.text;
+
+        const newFeedback = {
+            id: new Date().toISOString(),
+            email: email,
+            text: feedbackText
+        }
+
+        // store object in a DB or a file
+        const filePath = path.join(process.cwd(), 'data', 'feedback.json');
+        const fileData = fs.readFileSync(filePath);
+        const data = JSON.parse(fileData);
+        data.push(newFeedback);
+        fs.writeFileSync(filePath, JSON.stringify(data));
+        res.status(201).json({message: 'Success!', feedback: newFeedback});
+    } else {
+        res.status(200).json({message: 'This works!'});
+    }
 }
 
 export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,35 @@
 import {useRef} from 'react';
 
-const emailInputRef = useRef();
-const feedbackInputRef = useRef();
-
-function submitFormHandler(event) {
-    event.preventDefault();
-
-    const enteredEmail = emailInputRef.current.value;
-    const enteredFeedback = feedbackInputRef.current.value;
-}
 
 function HomePage() {
-  return (
-    <div>
-      <h1>The Home Page</h1>
-        <form onSubmit={submitFormHandler}>
-            <div>
-                <label htmlFor='email'>Your Email Address</label>
-                <input type='email' ref={emailInputRef}/>
-            </div>
-            <div>
-                <label htmlFor='feedback'>Your Feedback</label>
-                <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
-            </div>
-            <button>Send Feedback</button>
-        </form>
-    </div>
-  );
+    const emailInputRef = useRef();
+    const feedbackInputRef = useRef();
+
+    function submitFormHandler(event) {
+        event.preventDefault();
+
+        const enteredEmail = emailInputRef.current.value;
+        const enteredFeedback = feedbackInputRef.current.value;
+
+        fetch(); // {email: 'test@test.com', text: 'some feedback text'}
+    }
+
+    return (
+        <div>
+            <h1>The Home Page</h1>
+            <form onSubmit={submitFormHandler}>
+                <div>
+                    <label htmlFor='email'>Your Email Address</label>
+                    <input type='email' ref={emailInputRef}/>
+                </div>
+                <div>
+                    <label htmlFor='feedback'>Your Feedback</label>
+                    <textarea id='feedback' rows='5' ref={feedbackInputRef}></textarea>
+                </div>
+                <button>Send Feedback</button>
+            </form>
+        </div>
+    );
 }
 
 export default HomePage;
