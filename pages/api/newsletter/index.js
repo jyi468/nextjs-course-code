5e47596fd9d730d8053738359c45d9cfcda7f689
jyiwm <joshua.yi@warnermedia.com>
2021-07-22 11:39:24 -0400

    Running MongoDB Queries From Inside API Routes

--- pages/api/newsletter/index.js
+++ pages/api/newsletter/index.js
@@ -1,10 +1,23 @@
-const handler = (req, res) => {
+import {MongoClient} from "mongodb";
+
+const handler = async (req, res) => {
     if (req.method === 'POST') {
         const email = req.body.email;
+
+        const client = await MongoClient.connect(
+            'mongodb+srv://josh:ggzzmjkOe6CWHpMo@cluster0.ajkwa.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+        )
+        const db = client.db();
+
+        await db.collection('emails').insertOne({email: email});
+
+        await client.close();
+
         res.status(201).json({message: 'Successfully Registered', email});
     } else {
         res.status(200);
     }
+
 
 };
 
