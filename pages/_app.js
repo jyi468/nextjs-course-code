79c3125afb49a2fa4f6cfeb49c0110373f7bd675
jyiwm <joshua.yi@warnermedia.com>
2021-07-25 13:01:08 -0400

    Using Context Data in Components

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import Head from 'next/head';
 
 import Layout from '../components/layout/layout';
 import '../styles/globals.css';
-import Notification from "../components/ui/notification";
 import {NotificationContextProvider} from "../store/notification-context";
 
 function MyApp({Component, pageProps}) {
@@ -15,7 +14,6 @@ function MyApp({Component, pageProps}) {
                     <meta name='viewport' content='initial-scale=1.0, width=device-width'/>
                 </Head>
                 <Component {...pageProps} />
-                <Notification title="Test" message="This is a test." status="pending"/>
             </Layout>
         </NotificationContextProvider>
     );
